4a3767a64bf9ede0c8b1a0d1c965511f3b4b3cf1
czarp <piotr.czarny@pega.com>
2025-03-28 13:39:18 +0100

    TASK-1734650: chore(actions): Publish snapshot release to github

---
+++ .github/workflow-scripts/publish-snapshot.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+    --minor)
+        MINOR_VERSION="$2"
+        shift # past argument
+        shift # past value
+        ;;
+    --major)
+        MAJOR_VERSION="$2"
+        shift # past argument
+        shift # past value
+        ;;
+    -*)
+        echo "Unknown option $1"
+        exit 1
+        ;;
+    *)
+        POSITIONAL_ARGS+=("$1") # save positional arg
+        shift # past argument
+        ;;
+    esac
+done
+
+if [ -z "${MINOR_VERSION}" ] || [ -z "${MAJOR_VERSION}" ]; then
+    echo "Provide minor and major version e.g. $0 --major 1 --minor 0"
+    exit 1
+fi
+
+TIMESTAMP=$(date -u +%Y-%m-%d-%H-%M)
+LAST_VERSION=$(gh release list --exclude-drafts --exclude-pre-releases --json name --jq '.[0].name' --limit 1 | tr -d '[:space:]')
+
+echo "Last non-snapshot version: ${LAST_VERSION}"
+if [[ $(echo -n "${LAST_VERSION}" | wc -c | tr -d '[:space:]') -eq 0 ]]; then
+    echo "No last non-snapshot-version, assuming PATCH_VERSION=0"
+    PATCH_VERSION=0
+else
+    # Split string by '.'
+    IFS='.' read -ra ADDR <<< "${LAST_VERSION}"
+    PATCH_VERSION=${ADDR[2]}
+    echo "Getting patch version from last released version, PATCH_VERSION=${PATCH_VERSION}"
+fi
+
+VERSION="v${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}.snapshot-${TIMESTAMP}"
+echo "Will publish artifacts to release with version: ${VERSION}"
+
+gh release create "${VERSION}" --prerelease --generate-notes "ios-release/*.*" "android-release/*.*"
