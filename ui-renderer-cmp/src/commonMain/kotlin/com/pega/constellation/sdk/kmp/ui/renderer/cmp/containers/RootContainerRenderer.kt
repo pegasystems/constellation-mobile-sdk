1e1c2cffc853438bb9e6fa3ce904fd239b53ba24
mloct <tomasz.mlocek@pega.com>
2025-10-10 13:42:14 +0200

    TASK-1811575: Intercept window.alert and window.confirm JS calls

--- ui-renderer-cmp/src/commonMain/kotlin/com/pega/constellation/sdk/kmp/ui/renderer/cmp/containers/RootContainerRenderer.kt
+++ ui-renderer-cmp/src/commonMain/kotlin/com/pega/constellation/sdk/kmp/ui/renderer/cmp/containers/RootContainerRenderer.kt
@@ -3,12 +3,19 @@ package com.pega.constellation.sdk.kmp.ui.renderer.cmp.containers
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import com.pega.constellation.sdk.kmp.core.components.containers.RootContainerComponent
+import com.pega.constellation.sdk.kmp.core.components.widgets.AlertComponent
+import com.pega.constellation.sdk.kmp.ui.components.cmp.controls.form.Alert
+import com.pega.constellation.sdk.kmp.ui.components.cmp.controls.form.Confirm
 import com.pega.constellation.sdk.kmp.ui.renderer.cmp.ComponentRenderer
 import com.pega.constellation.sdk.kmp.ui.renderer.cmp.Render
 
 class RootContainerRenderer : ComponentRenderer<RootContainerComponent> {
+
     @Composable
     override fun RootContainerComponent.Render() {
+        val alert = context.componentManager.getAlertComponent()
+        val alertInfo = alert.info
+
         Box {
             viewContainer?.Render()
 //            Snackbar(
@@ -18,29 +25,29 @@ class RootContainerRenderer : ComponentRenderer<RootContainerComponent> {
 //                    .align(Alignment.BottomCenter)
 //                    .padding(bottom = 8.dp)
 //            )
-//            val alert = context.componentManager.getAlertComponent()
-//            alert.info?.let {
-//                when (it.type) {
-//                    AlertComponent.Type.CONFIRM -> Confirm(
-//                        message = it.message,
-//                        onConfirm = {
-//                            it.onConfirm()
-//                            alert.setAlertInfo(null)
-//                        },
-//                        onCancel = {
-//                            it.onCancel()
-//                            alert.setAlertInfo(null)
-//                        })
-//
-//                    AlertComponent.Type.ALERT -> Alert(
-//                        message = it.message,
-//                        onConfirm = {
-//                            it.onConfirm()
-//                            alert.setAlertInfo(null)
-//                        }
-//                    )
-//                }
-//            }
+            // val alert = context.componentManager.getAlertComponent()
+            alertInfo?.let {
+                when (it.type) {
+                    AlertComponent.Type.CONFIRM -> Confirm(
+                        message = it.message,
+                        onConfirm = {
+                            it.onConfirm()
+                            alert.setAlertInfo(null)
+                        },
+                        onCancel = {
+                            it.onCancel()
+                            alert.setAlertInfo(null)
+                        })
+
+                    AlertComponent.Type.ALERT -> Alert(
+                        message = it.message,
+                        onConfirm = {
+                            it.onConfirm()
+                            alert.setAlertInfo(null)
+                        }
+                    )
+                }
+            }
         }
     }
 }
