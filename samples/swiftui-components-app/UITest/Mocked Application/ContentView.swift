08accb68fa20ffda7ded795415ac93121b9a4186
czarp <piotr.czarny@pega.com>
2025-10-13 13:28:42 +0200

    TASK-1811945: Fix compilation issue

--- samples/swiftui-components-app/UITest/Mocked Application/ContentView.swift
+++ samples/swiftui-components-app/UITest/Mocked Application/ContentView.swift
@@ -54,19 +54,3 @@ struct ContentView: View {
         return SDKWrapper(sdk: sdk)
     }
 }
-
-struct EngineWebView: UIViewRepresentable {
-    private let webView: UIView
-
-    init(_ engine: WKWebViewBasedEngine) {
-        self.webView = engine.webView
-    }
-
-    func makeUIView(context: Context) -> UIView {
-        webView
-    }
-
-    func updateUIView(_ uiView: UIView, context: Context) {
-        // no-op
-    }
-}
