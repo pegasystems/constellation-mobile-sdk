3778db87f3331d933de341c9e410d20dabc2a545
pelcm <marek.pelc@pega.com>
2025-11-17 15:54:12 +0100

    ISSUE-137900: SimpleTableSelect WIP POC

--- scripts/dxcomponents/components/containers/templates/simple-table.component.js
+++ scripts/dxcomponents/components/containers/templates/simple-table.component.js
@@ -1,5 +1,5 @@
-import { Utils } from "../../../helpers/utils.js";
-import { BaseComponent } from "../../base.component.js";
+import {Utils} from "../../../helpers/utils.js";
+import {BaseComponent} from "../../base.component.js";
 
 const TAG = "[SimpleTableComponent]";
 
@@ -51,25 +51,95 @@ export class SimpleTableComponent extends BaseComponent {
             this.props.visible = this.utils.getBooleanValue(configProps.visibility);
         }
 
-        const { multiRecordDisplayAs } = configProps;
-        let { contextClass } = configProps;
+        const {
+            multiRecordDisplayAs,
+            label: labelProp,
+            propertyLabel,
+            displayMode,
+            fieldMetadata,
+            hideLabel,
+            parameters,
+            isDataObject,
+            type,
+            ruleClass,
+            authorContext,
+            name
+        } = configProps;
+        const label = labelProp || propertyLabel;
+
+        let {contextClass} = configProps;
         if (!contextClass) {
             let listName = this.pConn.getComponentConfig().referenceList;
             listName = PCore.getAnnotationUtils().getPropertyName(listName);
             contextClass = this.pConn.getFieldMetadata(listName)?.pageClass;
         }
-        if (multiRecordDisplayAs === "fieldGroup") {
-            const fieldGroupProps = { ...configProps, contextClass };
+        if (multiRecordDisplayAs === "fieldGroup") { // table, simpleTable
+            const fieldGroupProps = {...configProps, contextClass};
             this.childComponent = this.componentsManager.upsert(this.childComponent, "FieldGroupTemplate", [
                 this.pConn,
                 fieldGroupProps,
             ]);
             this.#sendPropsUpdate();
-        } else {
-            console.log(
-                TAG,
-                `Unsupported display mode: ${multiRecordDisplayAs}. ListView and SimpleTableManual are not supported yet.`
+        } else if (fieldMetadata && fieldMetadata.type === 'Page List' && fieldMetadata.dataRetrievalType === 'refer') {
+            const {
+                children: [{children: rawFields}],
+                parameters: rawParams
+            } = (this.pConn.getRawMetadata()).config;
+            const isDisplayModeEnabled = displayMode === 'DISPLAY_ONLY';
+            const propsToUse = {label, ...this.pConn.getInheritedProps()};
+            if (isDisplayModeEnabled && hideLabel) {
+                propsToUse.label = '';
+            }
+
+            const metaForListView = buildMetaForListView(
+                fieldMetadata,
+                rawFields,
+                type,
+                ruleClass,
+                name,
+                propsToUse.label,
+                isDataObject,
+                parameters // resolved params
             );
+
+            const metaForPConnect = JSON.parse(JSON.stringify(metaForListView));
+            // @ts-ignore - PCore.getMetadataUtils().getPropertyMetadata - An argument for 'currentClassID' was not provided.
+            metaForPConnect.config.parameters = rawParams ?? PCore.getMetadataUtils().getPropertyMetadata(name)?.datasource?.parameters;
+
+            const {referenceListStr: referenceList} = this.#getContext(this.pConn);
+            let requiredContextForQueryInDisplayMode = {};
+            if (isDisplayModeEnabled) {
+                requiredContextForQueryInDisplayMode = {
+                    referenceList
+                };
+            }
+            const options = {
+                context: this.pConn.getContextName(),
+                pageReference: this.pConn.getPageReference(),
+                ...requiredContextForQueryInDisplayMode
+            };
+
+            const listViewPConn = PCore.createPConnect({meta: metaForPConnect, options}).getPConnect();
+
+            const listViewProps = {
+                ...metaForListView.config,
+                displayMode,
+                fieldName: authorContext
+            };
+            // if (multiRecordDisplayAs === "simpleTable") {
+            //
+            // }
+            this.childComponent = this.componentsManager.upsert(this.childComponent, "ListView", [
+                listViewPConn,
+                listViewProps,
+            ]);
+            this.#sendPropsUpdate();
+        } else {
+            // console.warn(
+            //     TAG,
+            //     `Unsupported display mode: ${multiRecordDisplayAs}. ListView and SimpleTableManual are not supported yet.`
+            // );
+            this.childComponent = this.componentsManager.upsert(this.childComponent, "SimpleTableManual", [this.pConn])
         }
     }
 
@@ -80,8 +150,31 @@ export class SimpleTableComponent extends BaseComponent {
 
     #sendPropsUpdate() {
         this.props = {
-            child: this.childComponent.compId,
+            child: this.childComponent?.compId,
         };
         this.componentsManager.onComponentPropsUpdate(this);
     }
+
+    #getContext(thePConn) {
+        const contextName = thePConn.getContextName();
+        const pageReference = thePConn.getPageReference();
+        const {
+            readonlyContextList,
+            referenceList = readonlyContextList
+        } = thePConn.getStateProps()?.config || thePConn.getStateProps();
+
+        const pageReferenceForRows = referenceList.startsWith('.') ? `${pageReference}.${referenceList.substring(1)}` : referenceList;
+        const viewName = thePConn.viewName;
+
+        // removing "caseInfo.content" prefix to avoid setting it as a target while preparing pageInstructions
+        // skipping the removal as StateMachine itself is removing this case info prefix while preparing pageInstructions
+        // referenceList = pageReferenceForRows.replace(PCore.getConstants().CASE_INFO.CASE_INFO_CONTENT, '');
+
+        return {
+            contextName,
+            referenceListStr: referenceList,
+            pageReferenceForRows,
+            viewName
+        };
+    };
 }
