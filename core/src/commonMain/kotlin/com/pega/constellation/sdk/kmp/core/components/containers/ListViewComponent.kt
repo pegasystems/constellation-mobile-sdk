c4c92b738c034f08e42fde0e78c4da5d2398f609
lukjj-public-pega <jerzy.lukjaniec@pegasystems.com>
2025-11-26 16:06:43 +0100

    TASK-1830422: TASK-1830422: ListViewComponent now supports nested items. (#89)

--- core/src/commonMain/kotlin/com/pega/constellation/sdk/kmp/core/components/containers/ListViewComponent.kt
+++ core/src/commonMain/kotlin/com/pega/constellation/sdk/kmp/core/components/containers/ListViewComponent.kt
@@ -11,6 +11,7 @@ import com.pega.constellation.sdk.kmp.core.components.containers.ListViewCompone
 import com.pega.constellation.sdk.kmp.core.components.containers.ListViewComponent.SelectionMode.SINGLE
 import com.pega.constellation.sdk.kmp.core.components.getJSONArray
 import com.pega.constellation.sdk.kmp.core.components.getString
+import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.jsonPrimitive
@@ -39,10 +40,8 @@ class ListViewComponent(context: ComponentContext) : BaseComponent(context) {
         selectedItemIndex = props.getIntOrNull("selectedItemIndex")
         columnNames = props.getJSONArray("columnNames").map { it.jsonPrimitive.content }
         items = props.getJSONArray("items")
-            .map { item ->
-                item.jsonObject.entries
-                    .associate { it.key to it.value.jsonPrimitive.content }
-                    .let { Item(it) }
+            .map {
+                Item(it.toFoldedItemContent(emptyMap(), ""))
             }
     }
 
@@ -63,6 +62,23 @@ class ListViewComponent(context: ComponentContext) : BaseComponent(context) {
 
     private fun JsonObject.getIntOrNull(key: String): Int? =
         get(key)?.jsonPrimitive?.content?.takeIf { it.isNotBlank() }?.toIntOrNull()
+
+    private fun JsonElement.toFoldedItemContent(
+        initialResult: Map<String, String>,
+        currentPath: String
+    ): Map<String, String> =
+        if (this is JsonObject) {
+            jsonObject.entries.fold(initialResult) { accumulator, element ->
+                val nextPath = if (currentPath.isNotEmpty()) {
+                    "$currentPath.${element.key}"
+                } else {
+                    element.key
+                }
+                element.value.toFoldedItemContent(accumulator, nextPath)
+            }
+        } else {
+            initialResult + mapOf(currentPath to jsonPrimitive.content)
+        }
 
     private fun String.toSelectionMode() =
         when (this.uppercase()) {
