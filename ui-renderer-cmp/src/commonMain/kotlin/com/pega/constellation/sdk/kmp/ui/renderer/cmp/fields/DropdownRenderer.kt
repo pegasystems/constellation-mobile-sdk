9d977f5b4825b9f07688691c72bfb8b51c07f9d6
pelcm <marek.pelc@pega.com>
2025-11-27 19:31:25 +0100

    TASK-1786159: show display value for dropdown in readonly mode

--- ui-renderer-cmp/src/commonMain/kotlin/com/pega/constellation/sdk/kmp/ui/renderer/cmp/fields/DropdownRenderer.kt
+++ ui-renderer-cmp/src/commonMain/kotlin/com/pega/constellation/sdk/kmp/ui/renderer/cmp/fields/DropdownRenderer.kt
@@ -3,6 +3,7 @@ package com.pega.constellation.sdk.kmp.ui.renderer.cmp.fields
 import androidx.compose.runtime.Composable
 import com.pega.constellation.sdk.kmp.core.components.fields.SelectableComponent
 import com.pega.constellation.sdk.kmp.ui.components.cmp.controls.form.Dropdown
+import com.pega.constellation.sdk.kmp.ui.components.cmp.controls.form.FieldValue
 import com.pega.constellation.sdk.kmp.ui.components.cmp.controls.form.SelectableOption
 import com.pega.constellation.sdk.kmp.ui.renderer.cmp.ComponentRenderer
 import com.pega.constellation.sdk.kmp.ui.renderer.cmp.helpers.WithFieldHelpers
@@ -10,19 +11,23 @@ import com.pega.constellation.sdk.kmp.ui.renderer.cmp.helpers.WithFieldHelpers
 class DropdownRenderer : ComponentRenderer<SelectableComponent> {
     @Composable
     override fun SelectableComponent.Render() {
-        WithFieldHelpers {
-            Dropdown(
-                value = value,
-                label = label,
-                helperText = helperText,
-                validateMessage = validateMessage,
-                placeholder = placeholder,
-                required = required,
-                disabled = disabled,
-                readOnly = readOnly,
-                options = options.map { SelectableOption(it.key, it.label) },
-                onValueChange = { updateValue(it) }
-            )
-        }
+        WithFieldHelpers(
+            displayOnly = {
+                FieldValue(label, options.firstOrNull { it.key == value }?.label ?: "")
+            },
+            editable = {
+                Dropdown(
+                    value = value,
+                    label = label,
+                    helperText = helperText,
+                    validateMessage = validateMessage,
+                    placeholder = placeholder,
+                    required = required,
+                    disabled = disabled,
+                    readOnly = readOnly,
+                    options = options.map { SelectableOption(it.key, it.label) },
+                    onValueChange = { updateValue(it) }
+                )
+            })
     }
 }
