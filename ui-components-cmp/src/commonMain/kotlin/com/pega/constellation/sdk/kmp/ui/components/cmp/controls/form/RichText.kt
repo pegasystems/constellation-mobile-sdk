59c059567e936b0336db778dc3e90bfe128cd529
pelcm <marek.pelc@pega.com>
2026-02-18 10:53:55 +0100

    ISSUE-139957: fixing cursor position in input

--- ui-components-cmp/src/commonMain/kotlin/com/pega/constellation/sdk/kmp/ui/components/cmp/controls/form/RichText.kt
+++ ui-components-cmp/src/commonMain/kotlin/com/pega/constellation/sdk/kmp/ui/components/cmp/controls/form/RichText.kt
@@ -1,11 +1,14 @@
 package com.pega.constellation.sdk.kmp.ui.components.cmp.controls.form
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.TextFieldValue
 import be.digitalia.compose.htmlconverter.htmlToAnnotatedString
-import com.pega.constellation.sdk.kmp.ui.components.cmp.controls.form.internal.Input
+import com.pega.constellation.sdk.kmp.ui.components.cmp.controls.form.internal.HelperText
 
 @Composable
 fun RichText(
@@ -22,17 +25,35 @@ fun RichText(
         // Read-only RichText renders on web as display-only
         RichTextFieldValue(label, value)
     } else {
-        Input(
-            value = TextFieldValue(rememberAnnotated(value)),
-            label = label,
-            modifier = modifier,
-            helperText = helperText,
-            validateMessage = validateMessage,
-            required = required,
-            disabled = disabled,
-            readOnly = true, // not allowing editing in RichText for now
-            lines = 3
-        )
+        Column(modifier = modifier) {
+            // not allowing editing in RichText for now
+            OutlinedTextField(
+                value = TextFieldValue(rememberAnnotated(value)),
+                onValueChange = {},
+                modifier = Modifier.fillMaxWidth(),
+                label = {
+                    Label(
+                        label = label,
+                        hideLabel = false,
+                        required = required,
+                        disabled = disabled,
+                        readOnly = true
+                    )
+                },
+                placeholder = {  },
+                enabled = !disabled,
+                readOnly = true,
+                singleLine = false,
+                minLines = 3,
+                maxLines = 3,
+            )
+            HelperText(
+                text = helperText,
+                validateMessage = validateMessage,
+                disabled = disabled,
+                readOnly = true
+            )
+        }
     }
 }
 
--- ui-components-cmp/src/commonMain/kotlin/com/pega/constellation/sdk/kmp/ui/components/cmp/controls/form/internal/Input.kt
+++ ui-components-cmp/src/commonMain/kotlin/com/pega/constellation/sdk/kmp/ui/components/cmp/controls/form/internal/Input.kt
@@ -10,7 +10,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.text.input.TextFieldValue
 import com.pega.constellation.sdk.kmp.ui.components.cmp.controls.form.Label
 
 @Composable
@@ -33,51 +32,10 @@ internal fun Input(
     trailingIcon: @Composable (() -> Unit)? = null,
     interactionSource: MutableInteractionSource = remember { MutableInteractionSource() }
 ) {
-    Input(
-        value = TextFieldValue(value),
-        label = label,
-        modifier = modifier,
-        helperText = helperText,
-        validateMessage = validateMessage,
-        hideLabel = hideLabel,
-        placeholder = placeholder,
-        required = required,
-        disabled = disabled,
-        readOnly = readOnly,
-        onValueChange = onValueChange,
-        onFocusChange = onFocusChange,
-        lines = lines,
-        keyboardOptions = keyboardOptions,
-        leadingIcon = leadingIcon,
-        trailingIcon = trailingIcon,
-        interactionSource = interactionSource
-    )
-}
-
-@Composable
-internal fun Input(
-    value: TextFieldValue,
-    label: String,
-    modifier: Modifier = Modifier,
-    helperText: String = "",
-    validateMessage: String = "",
-    hideLabel: Boolean = false,
-    placeholder: String = "",
-    required: Boolean = false,
-    disabled: Boolean = false,
-    readOnly: Boolean = false,
-    onValueChange: (String) -> Unit = {},
-    onFocusChange: (Boolean) -> Unit = {},
-    lines: Int = 1,
-    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
-    leadingIcon: @Composable (() -> Unit)? = null,
-    trailingIcon: @Composable (() -> Unit)? = null,
-    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() }
-) {
     Column(modifier = modifier) {
         OutlinedTextField(
             value = value,
-            onValueChange = { onValueChange(it.text) },
+            onValueChange = onValueChange,
             modifier = Modifier
                 .fillMaxWidth()
                 .onFocusChanged { onFocusChange(it.isFocused) },
